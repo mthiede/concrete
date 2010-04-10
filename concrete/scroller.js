ca972a2348c01859dbf88b2524b25fd1832b7d5b
Martin Thiede <martin.thiede@gmx.de>
2010-04-10 17:24:12 +0200

    made scrolling to selected element also work within scroll containers

--- concrete/scroller.js
+++ concrete/scroller.js
@@ -6,17 +6,25 @@
 
 Concrete.Scroller = {};
 
+// scrolls the window or the first scrollable container by the smallest possible offset
+// which meximizes the visible part of element e 
 Concrete.Scroller.scrollTo = function(e) {
-	var vpLeft = document.viewport.getScrollOffsets().left;
-	var vpRight = document.viewport.getScrollOffsets().left + window.innerWidth;
-	var vpTop = document.viewport.getScrollOffsets().top;
-	var vpBottom = document.viewport.getScrollOffsets().top + window.innerHeight;
-	
-	var leftBorderDist = e.left()-vpLeft;
-	var rightBorderDist = vpRight-e.right();
-	var topBorderDist = e.top()-vpTop;
-	var bottomBorderDist = vpBottom-e.bottom();
-	
+
+  var isScrollable = function(f) {
+    return (f.getDimensions().width > f.up().getDimensions().width ||
+    f.getDimensions().height > f.up().getDimensions().height);
+  };
+  
+  var findScrollable = function(f) {
+    if (f.tagName.toUpperCase() == "BODY") return undefined;
+    if (isScrollable(f)) {
+      return f;
+    } 
+    else {
+      return findScrollable(f.up());
+    }
+  };
+
 	var maxScroll = function(negOffset, posOffset) {
 		if ((negOffset*-1) < posOffset) {
 			return negOffset*-1;
@@ -25,28 +33,74 @@ Concrete.Scroller.scrollTo = function(e) {
 			return posOffset;
 		}
 	}
-	
-	var scrollPosX;
-	var scrollPosY;
-	
-	if (leftBorderDist < 0 && rightBorderDist > 0) {
-		scrollPosX = vpLeft - maxScroll(leftBorderDist, rightBorderDist);
-	}
-	else if (rightBorderDist < 0 && leftBorderDist > 0) {
-		scrollPosX = vpLeft + maxScroll(rightBorderDist, leftBorderDist);
-	}
-	else {
-		scrollPosX = vpLeft;
-	}
 
-	if (topBorderDist < 0 && bottomBorderDist > 0) {
-		scrollPosY = vpTop - maxScroll(topBorderDist, bottomBorderDist);
-	}
-	else if (bottomBorderDist < 0 && topBorderDist > 0) {
-		scrollPosY = vpTop + maxScroll(bottomBorderDist, topBorderDist);
-	}
-	else {
-		scrollPosY = vpTop;
-	}
-	window.scrollTo(scrollPosX, scrollPosY);
+  var scrollable = findScrollable(e);
+
+  if (scrollable) {
+    var scrollContainer = scrollable.up();
+    var coff = scrollContainer.cumulativeOffset();
+    var cdim = scrollContainer.getDimensions();
+    var eoff = e.cumulativeOffset();
+    var edim = e.getDimensions();
+
+    var leftBorderDist = eoff.left - coff.left - scrollContainer.scrollLeft;
+    var rightBorderDist = coff.left + cdim.width - (eoff.left + edim.width) + scrollContainer.scrollLeft;
+    var topBorderDist = eoff.top - coff.top - scrollContainer.scrollTop;
+    var bottomBorderDist = coff.top + cdim.height - (eoff.top + edim.height) + scrollContainer.scrollTop;
+
+    if (leftBorderDist < 0 && rightBorderDist > 0) {
+      scrollContainer.scrollLeft = scrollContainer.scrollLeft - maxScroll(leftBorderDist, rightBorderDist);
+    }
+    else if (rightBorderDist < 0 && leftBorderDist > 0) {
+      scrollContainer.scrollLeft = scrollContainer.scrollLeft + maxScroll(rightBorderDist, leftBorderDist);
+    }
+    else {
+      // no scrolling required
+    }
+
+    if (topBorderDist < 0 && bottomBorderDist > 0) {
+      scrollContainer.scrollTop = scrollContainer.scrollTop - maxScroll(topBorderDist, bottomBorderDist);
+    }
+    else if (bottomBorderDist < 0 && topBorderDist > 0) {
+      scrollContainer.scrollTop = scrollContainer.scrollTop + maxScroll(bottomBorderDist, topBorderDist); 
+    }
+    else {
+      // no scrolling required
+    }
+  }
+  else {
+    var vpLeft = document.viewport.getScrollOffsets().left;
+    var vpRight = document.viewport.getScrollOffsets().left + window.innerWidth;
+    var vpTop = document.viewport.getScrollOffsets().top;
+    var vpBottom = document.viewport.getScrollOffsets().top + window.innerHeight;
+    
+    var leftBorderDist = e.left()-vpLeft;
+    var rightBorderDist = vpRight-e.right();
+    var topBorderDist = e.top()-vpTop;
+    var bottomBorderDist = vpBottom-e.bottom();
+    
+    var scrollPosX;
+    var scrollPosY;
+    
+    if (leftBorderDist < 0 && rightBorderDist > 0) {
+      scrollPosX = vpLeft - maxScroll(leftBorderDist, rightBorderDist);
+    }
+    else if (rightBorderDist < 0 && leftBorderDist > 0) {
+      scrollPosX = vpLeft + maxScroll(rightBorderDist, leftBorderDist);
+    }
+    else {
+      scrollPosX = vpLeft;
+    }
+
+    if (topBorderDist < 0 && bottomBorderDist > 0) {
+      scrollPosY = vpTop - maxScroll(topBorderDist, bottomBorderDist);
+    }
+    else if (bottomBorderDist < 0 && topBorderDist > 0) {
+      scrollPosY = vpTop + maxScroll(bottomBorderDist, topBorderDist);
+    }
+    else {
+      scrollPosY = vpTop;
+    }
+    window.scrollTo(scrollPosX, scrollPosY);
+  }
 }
