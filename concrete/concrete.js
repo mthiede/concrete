0be80b593f116d22d469e0d61bc0ca2d908b4c21
Meinte Boersma <meinte.boersma@gmail.com>
2011-09-27 22:15:34 +0200

    regularized indentation

--- concrete/concrete.js
+++ concrete/concrete.js
@@ -23,36 +23,36 @@
 
 var Concrete = {
   Version: '0.1.0',
-	require: function(r) {
-		document.write('<script type="text/javascript" src="'+r+'"><\/script>');
-	},
+  require: function(r) {
+    document.write('<script type="text/javascript" src="'+r+'"><\/script>');
+  },
   load: function() {
-  	// TODO: check prototype and scriptaculous version
+    // TODO: check prototype and scriptaculous version
     var js = /concrete\.js(\?.*)?$/;
     $$('head script[src]').findAll(function(s) {
       return s.src.match(js);
     }).each(function(s) {
       var path = s.src.replace(js, '');
       [
-				'editor',
-				'selector',
-				'scroller',
-				'element_extension',
-				'basic_inline_editor',
-				'inline_editor',
-				'template_provider',
-				'model_interface',
-				'metamodel_provider',
-				'identifier_provider',
-				'external_identifier_provider',
-				'constraint_checker',
-				'clipboard',
-				'helper'
+        'editor',
+        'selector',
+        'scroller',
+        'element_extension',
+        'basic_inline_editor',
+        'inline_editor',
+        'template_provider',
+        'model_interface',
+        'metamodel_provider',
+        'identifier_provider',
+        'external_identifier_provider',
+        'constraint_checker',
+        'clipboard',
+        'helper'
       ].each( function(include) { 
-      	Concrete.require(path + include + '.js');
-    	});
-		});
-	}
+        Concrete.require(path + include + '.js');
+      });
+    });
+  }
 }
 
 Concrete.load();
